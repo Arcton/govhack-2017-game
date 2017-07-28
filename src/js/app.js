b742f4881c7aaf9cf530bc6b84d16f2ad57639d3
Jeremy Symon <jtsymon@gmail.com>
2017-07-28 22:40:29 +1200

    Added main loop and example city/upgrade

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,36 @@
+import Tock from 'tocktimer';
+import City from './model/city';
+import Upgrade from './model/upgrade';
 import styles from './scss/app.scss';
+
+class ExampleUpgrade extends Upgrade {
+  constructor() {
+    super('Example Upgrade');
+    this.improve();
+  }
+
+  tick(elapsedTicks) {
+    return {
+      exampleResouce: elapsedTicks * this.level,
+    };
+  }
+}
+
+class ExampleCity extends City {
+  constructor() {
+    super('Example City', [new ExampleUpgrade()]);
+  }
+}
+
+const cities = [new ExampleCity()];
+
+const tick = new Tock({
+  interval: 100,
+  callback: () => {
+    cities.forEach((city) => {
+      console.log(city.tick());
+    });
+  },
+});
+
+tick.start();
