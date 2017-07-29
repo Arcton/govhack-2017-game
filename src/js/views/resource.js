c97a7203c210e752a490930466832d87fb8517f1
Nathaniel Watson <nathaniel@nwatson.nz>
2017-07-29 17:42:18 +1200

    Tweak resource bar styling

--- src/js/views/resource.js
+++ src/js/views/resource.js
@@ -2,12 +2,10 @@ import View from 'ampersand-view';
 
 export default View.extend({
   template: `<div class="resource" data-hook="container">
-    <span class="resource__name" data-hook="name"></span>:
-    <span class="resource__amount" data-hook="amount"></span>
+    <span class="resource__name" data-hook="name"></span><span class="resource__amount" data-hook="amount"></span>
   </div>`,
 
   bindings: {
-    'model.name': '[data-hook=name]',
     'model.amount': '[data-hook=amount]',
   },
 
